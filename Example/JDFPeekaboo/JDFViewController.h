8f67a832e82ea2947cc9af3530b14db99efb4f0b
Joe Fryer <joe.d.fryer@gmail.com>
2015-02-01 18:07:14 +0000

    Setup a demo.

--- Example/JDFPeekaboo/JDFViewController.h
+++ Example/JDFPeekaboo/JDFViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JDFViewController : UIViewController
+@interface JDFViewController : UITableViewController
 
 @end
--- Example/JDFPeekaboo/JDFViewController.m
+++ Example/JDFPeekaboo/JDFViewController.m
@@ -8,22 +8,58 @@
 
 #import "JDFViewController.h"
 
+// Peekaboo
+#import "JDFPeekabooCoordinator.h"
+
+
+static NSString *const JDFSampleViewControllerCellIdentifier = @"JDFSampleViewControllerCellIdentifier";
+
+
 @interface JDFViewController ()
 
+@property (nonatomic, strong) JDFPeekabooCoordinator *scrollCoordinator;
+
 @end
+
 
 @implementation JDFViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIColor *blueColour = [UIColor colorWithRed:44.0f/255.0f green:62.0f/255.0f blue:80.0f/255.0f alpha:1.0f];
+    
+    self.navigationController.toolbarHidden = NO;
+    self.navigationController.navigationBar.barTintColor = blueColour;
+    self.navigationController.toolbar.barTintColor = blueColour;
+    
+    self.scrollCoordinator = [[JDFPeekabooCoordinator alloc] init];
+    self.scrollCoordinator.scrollView = self.tableView;
+    self.scrollCoordinator.topView = self.navigationController.navigationBar;
+    self.scrollCoordinator.containingView = self.navigationController.view;
+    self.scrollCoordinator.topViewMinimisedHeight = 20.0f;
+    self.scrollCoordinator.bottomView = self.navigationController.toolbar;
+    
+    self.navigationItem.title = @"Peekaboo";
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:JDFSampleViewControllerCellIdentifier];
 }
 
-- (void)didReceiveMemoryWarning
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:JDFSampleViewControllerCellIdentifier forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld", (long)indexPath.row];
+    return cell;
 }
 
 @end
