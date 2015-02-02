175f7e97bad4610b9d10e9d03c66c3770976e803
Joe Fryer <joe.d.fryer@gmail.com>
2015-02-02 13:15:29 +0000

    Documentation.

--- Pod/Classes/JDFPeekabooCoordinator.h
+++ Pod/Classes/JDFPeekabooCoordinator.h
@@ -11,22 +11,54 @@
 
 @interface JDFPeekabooCoordinator : NSObject
 
-// Scroll View
+#pragma mark - Main Views
+
+/**
+ *  This is the scroll view that the @c JDFPeekabooCoordinator tracks. As the scrollView scrolls, the @c topView and @c bottomView are hidden & revealed appropriately.
+ */
 @property (nonatomic, weak) UIScrollView *scrollView;
 
-// Containing View
+/**
+ *  This is the view that contains the scrollView.
+ */
 @property (nonatomic, weak) UIView *containingView;
 
-// Top Views
+
+#pragma mark - Top Views
+
+/**
+ *  This view will be moved upwards so that it is hidden as the scrollView scrolls. This could be any view, including a UINavigationBar.
+ */
 @property (nonatomic, weak) UIView *topView;
+
+/**
+ *  This is an array of views (typically they are subviews of the top view) that will be dimmed, and eventually become invisible, as the topView is hidden. This is useful if you have some stuff on your top view that you want to be hidden when it is in its most hidden position.
+ */
 @property (nonatomic, strong) NSArray *topViewItems;
+
+/**
+ *  This is the height of the topView when it is in its most minimised position. The default is 20.0f;
+ */
 @property (nonatomic) CGFloat topViewMinimisedHeight;
 
-// Bottom Views
+
+#pragma mark - Bottom Views
+
+/**
+ *  This view will be moved upwards so that it is hidden as the scrollView scrolls. This could be any view, including a UINavigationBar.
+ */
 @property (nonatomic, weak) UIView *bottomView;
 
-// Methods
+#pragma mark - Methods
+
+/**
+ *  This method updates the Alpha of the @c topViewItems. You should call this method if you manually adjust the frame of the topView to make sure the alpha of the @c topViewItems is corrrect.
+ */
 - (void)setBarsNeedDisplay;
+
+/**
+ *  This method returns all of the views to their default position.
+ */
 - (void)fullyExpandViews;
 
 @end
